1dc1d3497e3e0960ee637e16b34f9ac1a0ab752e
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-02-26 12:17:16 +0100

    Develop a DOM updating algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,6 +19,8 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    resultsView.update(model.getSearchResultsPage());
+
     await model.loadRecipe(id);
 
     recipeView.render(model.state.recipe);
@@ -51,7 +53,7 @@ const controlPaginations = function (goToPage) {
 
 const controlServings = function (newServings) {
   model.updateServings(newServings);
-  recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
