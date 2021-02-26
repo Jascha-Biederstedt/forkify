ee8928a1af56b9feaddad5eaf84d5ada1811cd33
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-02-26 09:27:06 +0100

    Implement pagination

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,5 @@
-import { API_URL } from './config.js';
+import { async } from 'regenerator-runtime';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 
 export const state = {
@@ -6,6 +7,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    resultsPerPage: RES_PER_PAGE,
+    page: 1,
   },
 };
 
@@ -31,11 +34,11 @@ export const loadRecipe = async function (id) {
 };
 
 export const loadSearchResults = async function (query) {
+  state.search.page = 1;
   try {
     state.search.query = query;
 
     const data = await getJSON(`${API_URL}?search=${query}`);
-    console.log(data);
 
     state.search.results = data.data.recipes.map(rec => {
       return {
@@ -49,4 +52,12 @@ export const loadSearchResults = async function (query) {
     console.error(err);
     throw err;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
 };
